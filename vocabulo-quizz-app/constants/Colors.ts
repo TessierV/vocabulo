048c84455c85dd87ab362366eed0ba920bb4e3ce
unknown <5628@holbertonstudents.com>
2024-07-15 23:03:52 +0200

    add night/dark/color theme

--- vocabulo-quizz-app/constants/Colors.ts
+++ vocabulo-quizz-app/constants/Colors.ts
@@ -1,47 +1,54 @@
 /**
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-/* const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
- */
-
-// Colors.js
-const Colors = {
-  body: '#FFF',
-  darkShade: "#313941",
-  greyShade: "#A9A9A9",
-  lightBody: "#EFF5F2",
-  neutralBody: "#DAE7E2",
+export const color = {
+  white: '#FFF',
   lightGreen: "#C5E0E0",
   darkGreen: "#A2CBC4",
   lightCoral: "#E8B2A2",
   darkCoral: "#E18D73",
-  lightPlum: "#C39FD6",
-  darkPlum: "#BE9EDF",
+  lightPlum: "#D1BBE8",
+  darkPlum: "#AC83C8",
   lightYellow: "#EEDC99",
   darkYellow: "#E9A75D",
 };
 
-export default Colors;
+export const lightTheme = {
+  background: '#F5F2F1',
+  container: '#FFF',
 
+  title: '#3C385F',
+  title_reverse: '#F5F2F1',
 
-/* export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-}; */
+  text: '#243559',
+  text_reverse: '#EBECE9',
+
+  icon: '#262D34',
+  icon_title: '#262D34',
+
+  icon_container: '#6D768B',
+  text_container: '#6D768B',
+
+  icon_reverse: '#F5F2F1',
+};
+
+export const darkTheme = {
+  background: '#0D1320',
+  container: '#262D34',
+
+  title: 'yellow',
+  title_reverse: 'green',
+
+  text: '#DAE0E4',
+  text_reverse: 'plum',
+
+  icon: '#FFF',
+  icon_title: '#FFF',
+
+  icon_container: '#DAE0E4',
+  text_container: '#DAE0E4',
+
+  icon_reverse: 'purple',
+};
+
