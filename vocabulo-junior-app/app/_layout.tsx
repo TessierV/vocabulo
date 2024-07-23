29eb97310ca219dc9c955e475cf8f24cf156f284
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-07-23 17:02:09 +0200

    update file

--- vocabulo-junior-app/app/_layout.tsx
+++ vocabulo-junior-app/app/_layout.tsx
@@ -1,7 +1,26 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import React, { useEffect, useState } from "react";
+import useCustomFonts from '@/constants/useCustomFonts';
 
 export default function RootLayout() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      const fontsSuccessfullyLoaded = await useCustomFonts();
+      if (fontsSuccessfullyLoaded) {
+        setFontsLoaded(true);
+      } else {
+      }
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
