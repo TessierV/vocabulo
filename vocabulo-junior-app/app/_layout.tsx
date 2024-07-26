e0f2e2cdbf9fce17ecae1ed29f1639d2890da420
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-07-26 14:50:49 +0200

    add signup and login routers

--- vocabulo-junior-app/app/_layout.tsx
+++ vocabulo-junior-app/app/_layout.tsx
@@ -1,7 +1,13 @@
-import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
 import React, { useEffect, useState } from "react";
 import useCustomFonts from '@/constants/useCustomFonts';
+import { createStackNavigator } from "@react-navigation/stack";
+import Detail from "./auth/Signup/Detail";
+import Dashboard from "./auth/Signup/Dashboard";
+import Login from "./auth/Login/Login";
+import Signup from "./auth/Signup/Signup";
+/*import { Stack } from "expo-router";*/
+
+const Stack = createStackNavigator();
 
 export default function RootLayout() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -11,7 +17,6 @@ export default function RootLayout() {
       const fontsSuccessfullyLoaded = await useCustomFonts();
       if (fontsSuccessfullyLoaded) {
         setFontsLoaded(true);
-      } else {
       }
     }
     loadFonts();
@@ -22,8 +27,14 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    /*<Stack >
+      <Stack.Screen name="index"></Stack.Screen>
+    </Stack>*/
+    <Stack.Navigator initialRouteName="auth/Signup/Signup">
+      <Stack.Screen name="auth/Signup/Signup" component={Signup} options={{ headerShown: false }} />
+      <Stack.Screen name="auth/Signup/Detail" component={Detail} options={{ headerShown: false }} />
+      <Stack.Screen name="auth/Signup/Dashboard" component={Dashboard} options={{ headerShown: false }} />
+      <Stack.Screen name="auth/Login/Login" component={Login} options={{ headerShown: false }} />
+    </Stack.Navigator>
   );
 }
