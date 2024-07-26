12c83f0127beef8852028787456939d7a4b1f791
unknown <5628@holbertonstudents.com>
2024-07-26 14:58:19 +0200

    put text in variable file

---
+++ vocabulo-quizz-app/constants/texts.js
@@ -0,0 +1,54 @@
+// src/constants/texts.js
+export const texts = {
+
+    profilScreen: {
+        banner: {
+            title: 'Titre Profil bannière',
+            text: 'Je suis le texte de la bannière Profil',
+            popup: {
+                title: 'Je suis le titre de la popup de Profil',
+                text: 'Je suis le texte de la popup de Profil',
+                button: 'Fermer',
+            },
+        },
+
+        section: {
+            title: 'Profil Titre Section',
+            editProfile: "Profil modification",
+            changePassword: "Changer de mot de passe",
+        },
+    },
+
+    parameterScreen: {
+        banner: {
+            title: 'Titre Parametres bannière',
+            text: 'Je suis le texte de la bannière Parametres',
+            popup: {
+                title: 'titre popup de Parametres',
+                text: 'Je suis le texte de la popup de Parametres',
+                button: 'Fermer',
+            },
+        },
+
+        section: {
+            title: 'Profil',
+            editProfile: "Modifier le profil",
+            changePassword: "Modifier le mot de passe",
+        },
+
+        section_second: {
+            title: 'Personnalisation',
+            themeLight: "Thème clair",
+            themeDark: "Thème sombre",
+            sizeText: "Taille de texte",
+        },
+
+        section_third: {
+            title: 'Settings',
+            ask: "FAQ",
+            contact: "Contactez-nous",
+            version: "Version",
+
+        },
+    },
+};
