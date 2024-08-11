0d69580000d94d2a8837f0f6232ad0610f94e78d
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-08-11 13:35:00 +0200

    home page designed

--- vocabulo-junior-app/app/screens/HomeScreen.tsx
+++ vocabulo-junior-app/app/screens/HomeScreen.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import TopNavBar from '@/components/navigation/TopNavBar'
-import { Colors } from '@/constants/Colors'
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+import TopNavBar from '@/components/navigation/TopNavBar';
+import GoToStart from '@/components/HomeScreen/GoToStart';
+import { Colors } from '@/constants/Colors';
+import DisplayProfile from '@/components/HomeScreen/DisplayProfile';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <TopNavBar title="Accueil" tintColor={Colors.darkPlum} color={Colors.darkPlum} />
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <TopNavBar title="Accueil" tintColor={Colors.darkBlue} color={Colors.darkBlue} />
+      <View style={styles.DisplayProfileComponent}>
+      <DisplayProfile />
+      </View>
+      <View style={styles.GoToStartComponent}>
+        <GoToStart />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+  },
+  DisplayProfileComponent: {
+    marginTop: 20
+  },
+  GoToStartComponent: {
+    flex: 1,
+    marginTop: 100
+  },
+});
