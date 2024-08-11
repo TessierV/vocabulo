5ee07dbc0cbf5f5cc656156503c507000cbd6520
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-08-11 19:24:22 +0200

    display tabs files with screens

--- vocabulo-junior-app/app/(tabs)/Dictionnary.tsx
+++ vocabulo-junior-app/app/(tabs)/Dictionnary.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import DictionnaryScreen from '../screens/DictionnaryScreen'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import DictionnaryScreen from '../screens/DictionnaryScreen';
+
 
 export default function Dictionnary() {
   return (
-    <View>
+    <View style={styles.container}>
       <DictionnaryScreen />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- vocabulo-junior-app/app/(tabs)/TakePhoto.tsx
+++ vocabulo-junior-app/app/(tabs)/TakePhoto.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import TakePhotoScreen from '../screens/TakePhotoScreen'
 
 export default function TakePhoto() {
   return (
-    <View>
+    <View style={styles.container}>
       <TakePhotoScreen />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
