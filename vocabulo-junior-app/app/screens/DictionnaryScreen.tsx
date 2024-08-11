23e605f3a33ce128e6dcb95266a291abafff7a15
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-08-11 13:32:54 +0200

    add design alphabet for dictionnary

--- vocabulo-junior-app/app/screens/DictionnaryScreen.tsx
+++ vocabulo-junior-app/app/screens/DictionnaryScreen.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+import TopNavBar from '@/components/navigation/TopNavBar';
+import SortByAlphabet from '@/components/DictionnaryScreen/SortByAlphabet';
+import { Colors } from '@/constants/Colors';
+import DisplayDictionnary from '@/components/DictionnaryScreen/DisplayDictionnary';
 
 export default function DictionnaryScreen() {
   return (
-    <View>
-      <Text>DictionnaryScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <TopNavBar title="Dictionnaire" tintColor={Colors.darkGreen} color={Colors.darkGreen} />
+      <View style={styles.SortByAlphabetComponent}>
+        <SortByAlphabet />
+      </View>
+      <View style={styles.DisplayDictionnaryComponent}>
+      <DisplayDictionnary />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+  },
+  DisplayDictionnaryComponent: {
+    marginTop: 100
+  },
+  SortByAlphabetComponent: {
+    flex: 1,
+    marginTop: 0
+  },
+});
---
+++ vocabulo-junior-app/components/DictionnaryScreen/SortByAlphabet.tsx
@@ -0,0 +1,68 @@
+import { View, TouchableOpacity, StyleSheet, FlatList, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from '@/constants/Colors';
+import { ButtonText, InformationText } from '@/constants/StyledText';
+
+
+type Letter = string;
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+export default function SortByAlphabet() {
+    const [selectedLetter, setSelectedLetter] = useState<Letter | null>(null);
+
+    const handlePress = (letter: Letter) => {
+        setSelectedLetter(selectedLetter === letter ? null : letter);
+    };
+
+    const renderItem = ({ item }: { item: Letter }) => {
+        const isSelected = item === selectedLetter;
+        return (
+            <TouchableOpacity
+                onPress={() => handlePress(item)}
+                style={[styles.button, isSelected && styles.selectedButton]}
+            >
+                <InformationText style={styles.buttonText}>
+                    {item}
+                </InformationText>
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={alphabet}
+                renderItem={renderItem}
+                keyExtractor={(item) => item}
+                numColumns={10}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '90%',
+        height: 'auto',
+        justifyContent: 'space-between',
+        marginHorizontal: 'auto',
+        top: 50,
+    },
+    button: {
+        backgroundColor: Colors.white,
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '10.5%',
+        height: 45,
+        margin: '1%',
+    },
+    selectedButton: {
+        backgroundColor: Colors.neutralGreen,
+    },
+    buttonText: {
+        color: Colors.black,
+    },
+});
