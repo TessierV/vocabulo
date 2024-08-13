69924ae93a438ea0f205bdf82d1151a30df77b3f
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-08-13 16:36:58 +0200

    test display data

--- vocabulo-junior-app/app/screens/DictionnaryScreen.tsx
+++ vocabulo-junior-app/app/screens/DictionnaryScreen.tsx
@@ -1,28 +1,40 @@
 import { View, StyleSheet, SafeAreaView } from 'react-native';
 import React from 'react';
 import TopNavBar from '@/components/Navigation/TopNavBar';
-import SortByAlphabet from '@/components/DictionnaryScreen/SortByAlphabet';
+import SortByAlphabet from '@/components/Dictionnary/SortByAlphabet';
 import { Colors } from '@/constants/Colors';
-import DisplayDictionnary from '@/components/DictionnaryScreen/DisplayDictionnary';
+import DictionnaryCard from '@/components/Dictionnary/DictionnaryCard';
+import SortByCategory from '@/components/Dictionnary/SortByCategory';
 
 export default function DictionnaryScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <TopNavBar title="Dictionnaire" tintColor={Colors.darkCoral} color={Colors.darkCoral} />
-      <View style={styles.SortByAlphabetComponent}>
-        <SortByAlphabet />
-      </View>
-    </SafeAreaView>
-  );
-}
+      <SafeAreaView style={styles.container}>
+        <TopNavBar title="Accueil" tintColor={Colors.darkCoral} color={Colors.darkCoral} />
+        <View style={styles.SortByCategoryComponent}>
+          <SortByCategory />
+        </View>
+        <View style={styles.SortByAphabetComponent}>
+          <SortByAlphabet />
+        </View>
+        <View style={styles.DictionnaryCardComponent}>
+          <DictionnaryCard />
+        </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.lightGrey,
-  },
-  SortByAlphabetComponent: {
-    flex: 1,
-    marginTop: 0
-  },
-});
+      </SafeAreaView>
+    );
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: Colors.lightGrey,
+    },
+    SortByAphabetComponent: {
+      marginTop: 20,
+    },
+    SortByCategoryComponent: {
+      marginTop: 20,
+    },
+    DictionnaryCardComponent: {
+      marginTop: 20,
+    },
+  });
---
+++ vocabulo-junior-app/components/Dictionnary/DictionnaryCard.tsx
@@ -0,0 +1,48 @@
+// WordCard.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Colors } from '@/constants/Colors'; // Assurez-vous que le chemin est correct
+
+interface DictionnaryCard {
+    mot: string;
+    categorie: string;
+    definition: string;
+    urlVideoDef: string;
+    urlVideoMot: string;
+    urlSource: string;
+}
+
+const WordCard: React.FC<DictionnaryCard> = ({ mot, categorie, definition, urlVideoDef, urlVideoMot, urlSource }) => (
+    <View style={styles.card}>
+        <Text style={styles.cardTitle}>{mot}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>Catégorie:</Text> {categorie}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>Définition:</Text> {definition}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>URL Vidéo Définition:</Text> {urlVideoDef}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>URL Vidéo Mot:</Text> {urlVideoMot}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>URL Source:</Text> {urlSource}</Text>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: Colors.white,
+        padding: 15,
+        margin: 5,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'red',
+    },
+    cardTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    cardText: {
+        fontSize: 16,
+        marginVertical: 2,
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+});
+
+export default WordCard;
