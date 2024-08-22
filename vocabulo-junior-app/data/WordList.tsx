b2c5f166145b61c34726d2fc53960deb2337f2de
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-08-22 16:37:38 +0200

    manage special characters

--- vocabulo-junior-app/data/WordList.tsx
+++ vocabulo-junior-app/data/WordList.tsx
@@ -1,53 +1,44 @@
 import scannedTextData from './scannedTextData';
 
-
 /**
- * Fonction qui prend une chaîne de texte et renvoie une liste de mots.
- * @param text La chaîne de texte à analyser.
- * @returns Un tableau de mots extraits du texte.
+ * Function to extract words from a given text.
+ * @param scannedTextData The text from which to extract words.
+ * @returns An array of words extracted from the text.
  */
-export function extractWordsFromText(text: string): string[] {
-  // Utiliser une expression régulière pour séparer les mots en éliminant les ponctuations.
-  return text
-    .toLowerCase() // Optionnel : mettre tout en minuscules pour normaliser.
-    .match(/\b\w+\b/g) || []; // \b\w+\b correspond aux mots.
+export function extractWordsFromText(scannedTextData: string): string[] {
+    return scannedTextData
+        .toLowerCase()
+        .match(/[à-ÿ\w'-]+/g) || [];
 }
 
-// Extraire les mots du texte importé
 export const wordsListFromText = extractWordsFromText(scannedTextData);
 
-
-// dictionaryProcessor.ts
 
 import dictionaryData from './dictionaryData';
 
 /**
- * Fonction pour extraire les mots d'une chaîne CSV.
- * @param csvData La chaîne de texte CSV contenant les données du dictionnaire.
- * @returns Un tableau de mots extraits de la colonne 'mot'.
+ * Function to extract words from dictionary data.
+ * @param dictionaryData The dictionary data in CSV format.
+ * @returns An array of words extracted from the dictionary.
  */
-export function extractWordsFromDictionary(csvData: string): string[] {
-  // Diviser le texte CSV en lignes
-  const lines = csvData.trim().split('\n');
-  
-  // Extraire l'en-tête (première ligne)
-  const header = lines[0].split(',');
+export function extractWordsFromDictionary(dictionaryData: string): string[] {
+    const lines = dictionaryData.trim().split('\n');
+    const header = lines[0].split(',');
 
-  // Trouver l'index de la colonne 'mot'
-  const wordIndex = header.indexOf('mot');
 
-  if (wordIndex === -1) {
-    throw new Error('Colonne "mot" non trouvée dans les données CSV.');
-  }
+    const wordIndex = header.indexOf('mot');
 
-  // Extraire les mots (ignorer la première ligne d'en-tête)
-  const words = lines.slice(1).map(line => {
-    const columns = line.split(',');
-    return columns[wordIndex].trim();
-  });
+    if (wordIndex === -1) {
+        throw new Error('Column "mot" not found in dictionary data.');
+    }
 
-  return words;
+
+    const words = lines.slice(1).map(line => {
+        const columns = line.split(',');
+        return columns[wordIndex].trim();
+    });
+
+    return words;
 }
 
-// Utiliser la fonction pour extraire les mots du texte importé
 export const wordsListFromDictionary = extractWordsFromDictionary(dictionaryData);
