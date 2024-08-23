f2a602bf3e4234c18ee540252ca32bcf88aca153
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-08-23 10:42:52 +0200

    saved index file

--- vocabulo-junior-app/app/index.tsx
+++ vocabulo-junior-app/app/index.tsx
@@ -1,11 +1,23 @@
-import { Stack, useNavigation } from 'expo-router';
+import { Stack, Tabs, useNavigation } from 'expo-router';
 import { Text, View } from 'react-native';
 import { useEffect } from 'react';
 import HomeScreen from './screens/HomeScreen';
-import TopNavBar from '@/components/navigation/TopNavBar';
+import TopNavBar from '@/components/Navigation/TopNavBar';
+import LoginScreen from './screens/LoginScreen';
+import DictionnaryScreen from './screens/DictionnaryScreen';
+import Dictionnary from './(tabs)/Dictionnary';
+import TakePhoto from './(tabs)/TakePhoto';
+import TakePhotoScreen from './screens/TakePhotoScreen';
+import ParametersScreen from './screens/SettingsScreen';
+import ScannedTextScreen from './screens/ScannedTextScreen';
+import TabLayout from './(tabs)/_layout';
+import SplashScreen from './screens/SplashScreen';
+import LogoSplash from '@/components/Splash/LogoSplash';
+import AllFilters from '@/components/Dictionnary/AllFilters';
+import DisplayCommunWords from '@/components/ScannedText/DisplayCommonWords';
+import ScannedTextAnalyzed from '@/components/ScannedText/ScannedTextAnalyzed';
 
 export default function Index() {
-
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -18,7 +30,7 @@ export default function Index() {
         flex: 1
       }}
     >
-      <HomeScreen />
+      <Dictionnary/>
     </View>
   );
 }
