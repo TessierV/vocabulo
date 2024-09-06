00b7e95bb4eb410b6ae52a5f55b0492a7653384e
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-06 11:38:44 +0200

    add padding

--- vocabulo-junior-app/app/screens/ScannedTextScreen.tsx
+++ vocabulo-junior-app/app/screens/ScannedTextScreen.tsx
@@ -1,14 +1,17 @@
-import { View, StyleSheet, SafeAreaView } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import React from 'react'
+import OCRScannedTextScreen from '@/components/ScannedText/OCRScannedTextScreen';
 import TopNavBar from '@/components/Navigation/TopNavBar';
 import { Colors } from '@/constants/Colors';
-import ScannedTextAnalyzed from '@/components/ScannedText/ScannedTextAnalyzed';
+
 
 export default function ScannedTextScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <TopNavBar title="Texte scanné" tintColor={Colors.darkGreen} color={Colors.darkGreen} />
-      <View style={styles.ScannedTextAnalyzed}><ScannedTextAnalyzed /></View>
+      <View style={styles.OCRScannedTextScreenComponent}>
+      <OCRScannedTextScreen />
+      </View>
     </SafeAreaView>
   );
 }
@@ -16,10 +19,10 @@ export default function ScannedTextScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.lightGrey,
   },
-  ScannedTextAnalyzed : {
-    flex: 1,
-    marginTop: -100
-  }
+  OCRScannedTextScreenComponent: {
+    marginTop: -50,
+    flex: 1
+  },
+
 });
