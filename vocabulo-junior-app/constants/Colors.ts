fa3cc7f9878b94bd570fe171dc62b09a4da52fe5
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-07 18:32:06 +0200

    add new styles

--- vocabulo-junior-app/constants/Colors.ts
+++ vocabulo-junior-app/constants/Colors.ts
@@ -10,8 +10,8 @@ export const Colors = {
   white: "#ffffff",
   whiteTransparent: "#ffffff50",
 
-  black: "000000",
-  blackTransparent: "00000050",
+  black: "#000000",
+  blackTransparent: "#00000050",
 
   lightGrey: "#EBEBEB",
   grey: "#7F7F7F",
@@ -35,6 +35,21 @@ export const Colors = {
   lightPlum: "#EFE0FF",
   lightPlumTransparent: "#EFE0FF50",
 
+  // Added pastel colors
+  pastelPink: "#F6C1C1",
+  pastelOrange: "#F6C1A7",
+  pastelYellow: "#F9F4A7",
+  pastelGreen: "#C1F6C1",
+  pastelBlue: "#A7D9F6",
+  pastelPurple: "#D4A7F6",
+  pastelPeach: "#F6D2B0",
+  pastelLavender: "#D2A7F6",
+  pastelMint: "#A7F6D2",
+  pastelLilac: "#D2A7F6",
+  pastelSalmon: "#F6A7A7",
+  pastelTurquoise: "#A7F6F6",
+  pastelBeige: "#F6F1E7",
+
   light: {
     text: '#11181C',
     background: '#fff',
@@ -50,5 +65,22 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+  },
+
+  // Grammatical categories of words
+  grammaticalCategories: {
+    pastelPink: "adjectif",
+    pastelOrange: "adjectif",
+    pastelYellow: "adjectif",
+    pastelGreen: "adjectif",
+    pastelBlue: "adjectif",
+    pastelPurple: "adjectif",
+    pastelPeach: "adjectif",
+    pastelLavender: "adjectif",
+    pastelMint: "adjectif",
+    pastelLilac: "adjectif",
+    pastelSalmon: "adjectif",
+    pastelTurquoise: "adjectif",
+    pastelBeige: "adjectif",
   },
 };
