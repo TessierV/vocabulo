724f8bd3c7d513db58992b5cbe637a760e0f11b8
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 11:51:30 +0200

    LoginLayout component to manage the navigation for login-related screens

--- vocabulo-junior-app/app/auth/authLogin/_layout.tsx
+++ vocabulo-junior-app/app/auth/authLogin/_layout.tsx
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from "react";
-import useCustomFonts from '@/constants/useCustomFonts';
+// This file defines the LoginLayout component to manage the navigation for login-related screens.
+
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+
+// Import the screen components used in the navigator
 import HomeScreen from "@/app/screens/HomeScreen";
 import Signup from "../authSignup/Signup";
 import Login from "../authLogin/Login";
 
+// Create a Stack Navigator instance
 const Stack = createStackNavigator();
 
 export default function LoginLayout() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadFonts() {
-      const fontsSuccessfullyLoaded = await useCustomFonts();
-      if (fontsSuccessfullyLoaded) {
-        setFontsLoaded(true);
-      }
-    }
-    loadFonts();
-  }, []);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
