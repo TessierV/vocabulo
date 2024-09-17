c7f37a2808780df2c9c94be0d99b7341dac73fbc
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 14:26:19 +0200

    splash screen for the application

--- vocabulo-junior-app/app/screens/SplashScreen.tsx
+++ vocabulo-junior-app/app/screens/SplashScreen.tsx
@@ -1,10 +1,17 @@
+// SplashScreen.js
+// This file defines the splash screen for the application,
+// displaying animated wave and logo components before navigating to the login screen.
+
 import React, { useEffect, useRef } from 'react';
 import { View, Animated, StyleSheet } from 'react-native';
-import LogoSplash from '../../components/Splash/LogoSplash';
-import WaveSplash from '../../components/Splash/WaveSplash';
-import { Colors } from '@/constants/Colors';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native'; // Hook for navigation
+import { StackNavigationProp } from '@react-navigation/stack'; // Type for navigation prop
+
+import { Colors } from '@/constants/Colors'; // Importing colors defined in constants
+
+import LogoSplash from '../../components/Splash/LogoSplash'; // Importing the LogoSplash component
+import WaveSplash from '../../components/Splash/WaveSplash'; // Importing the WaveSplash component
+
 
 type RootStackParamList = {
   SplashScreen: undefined;
@@ -15,35 +22,40 @@ type RootStackParamList = {
 type SplashScreenNavigationProp = StackNavigationProp<RootStackParamList, 'SplashScreen'>;
 
 export default function SplashScreen() {
-  const navigation = useNavigation<SplashScreenNavigationProp>();
-  const movewaveAnim = useRef(new Animated.Value(1400)).current;
+  const navigation = useNavigation<SplashScreenNavigationProp>(); // Hook for navigation
+  const movewaveAnim = useRef(new Animated.Value(1400)).current; // Reference for wave animation
 
   useEffect(() => {
+    // Animate the wave splash component
     Animated.timing(movewaveAnim, {
       toValue: 300,
       duration: 7000,
       useNativeDriver: false,
     }).start();
 
+    // Navigate to the LoginScreen after 8 seconds
     const timeout = setTimeout(() => {
       navigation.navigate("LoginScreen");
     }, 8000);
 
+    // Clear timeout on component unmount
     return () => clearTimeout(timeout);
   }, [movewaveAnim, navigation]);
 
   return (
     <View style={styles.container}>
+      {/* Animated WaveSplash component */}
       <Animated.View
         style={[
           styles.waveContainer,
           {
-            transform: [{ translateY: movewaveAnim }],
+            transform: [{ translateY: movewaveAnim }], // Apply translation animation
           },
         ]}
       >
         <WaveSplash />
       </Animated.View>
+      {/* LogoSplash component */}
       <LogoSplash />
     </View>
   );
