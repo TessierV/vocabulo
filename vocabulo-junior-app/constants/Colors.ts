9ec4533113f960c9e53cb7a7866f210633f6192f
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 15:37:01 +0200

    colors used in the application

--- vocabulo-junior-app/constants/Colors.ts
+++ vocabulo-junior-app/constants/Colors.ts
@@ -1,10 +1,5 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+// Below are the colors that are used in the app.
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
 
 export const Colors = {
   white: "#ffffff",
@@ -26,21 +21,11 @@ export const Colors = {
   lightGreen: "#DCE6E2",
 
   darkBlue: "#7DAED6",
-  darkBlueTransparent: "#7DAED680",
   neutralBlue: "#ABCDE8",
-  neutralBlueTransparent: "#ABCDE880",
   lightBlue: "#D1E6F6",
 
   darkPlum: "#AC83C8",
   neutralPlum: "#BE9EDF",
   lightPlum: "#EFE0FF",
   lightPlumTransparent: "#EFE0FF50",
-
-  CATyellow: "#FFD400",
-  CATblue: "#A7C4D9",
-  CATdarkblue: "#4A6A8C",
-  CATred: "#F5A3A3",
-  CATorange: "#FFB84D",
-  CATpink: "#FBB6D7",
-  CATgreen: "#9AE1A0",
 };
