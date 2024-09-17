fcd94325d0bcd8161cd277930c6bdc4a0e0e74b4
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 11:11:08 +0200

    TakePhoto page, linked to the tab navigation bar and rendering TakePhotoScreen

--- vocabulo-junior-app/app/(tabs)/TakePhoto.tsx
+++ vocabulo-junior-app/app/(tabs)/TakePhoto.tsx
@@ -1,9 +1,13 @@
-import { View, Text, StyleSheet } from 'react-native'
+// This file defines the TakePhoto page, which is linked to the tab navigation bar and displays the content of TakePhotoScreen.
+
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
-import TakePhotoScreen from '../otherScreens/TakePhotoScreen';
+
+import TakePhotoScreen from '../otherScreens/TakePhotoScreen';  // Importing the TakePhotoScreen component
 
 export default function TakePhoto() {
   return (
+    // Main container view that wraps the TakePhotoScreen component
     <View style={styles.container}>
       <TakePhotoScreen />
     </View>
