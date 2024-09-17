aca8fcd5b382b8c53ad231650c4bbc5da76f819e
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 14:25:15 +0200

    login screen in the application

--- vocabulo-junior-app/app/screens/LoginScreen.tsx
+++ vocabulo-junior-app/app/screens/LoginScreen.tsx
@@ -1,39 +1,51 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+// LoginScreen.js
+// This file defines the login screen in the application,
+// displaying a background image, welcome text, pictograms, and login/signup buttons.
+
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
-import { Colors } from '@/constants/Colors';
-import { useRouter } from 'expo-router';
-import BackgroundLogin from '../../components/auth/backgroundLogin'
-import { ButtonText, BigTitle } from '@/constants/StyledText';
+import { useRouter } from 'expo-router'; // Importing the router for navigation
+
+import { ButtonText, BigTitle } from '@/constants/StyledText'; // Importing styled text defined in constants
+import { Colors } from '@/constants/Colors'; // Importing colors defined in constants
+
+import BackgroundLogin from '../../components/auth/backgroundLogin'; // Importing the BackgroundLogin component
 
 
 export default function LoginScreen() {
-    const router = useRouter();
+    const router = useRouter(); // Hook for navigation
 
     return (
         <View>
-            <View style={{ height: '60%' }}>
+            {/* BackgroundLogin component taking up 60% of the screen height */}
+            <View style={styles.backgroundContainer}>
                 <BackgroundLogin />
             </View>
             <View style={styles.container}>
+                {/* Displaying the welcome text */}
                 <BigTitle style={styles.welcomeText}>Bienvenue</BigTitle>
+                {/* Container for pictograms arranged in a row */}
                 <View style={styles.pictoLSFContainer}>
-                    <Image source={require('./../../assets/images/pictoLSF/B.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/I.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/E.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/N.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/V.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/E.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/N.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/U.png')} style={styles.pictoLSF}/>
-                    <Image source={require('./../../assets/images/pictoLSF/E.png')} style={styles.pictoLSF}/>
+                    <Image source={require('./../../assets/images/pictoLSF/B.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/I.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/E.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/N.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/V.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/E.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/N.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/U.png')} style={styles.pictoLSF} />
+                    <Image source={require('./../../assets/images/pictoLSF/E.png')} style={styles.pictoLSF} />
                 </View>
+                {/* Container for the login and signup buttons */}
                 <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={() => router.push('./../auth/authLogin/Login')}>
-                    <ButtonText style={styles.buttonText}>Se connecter</ButtonText>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => router.push('./../auth/authSignup/Signup')}>
-                    <ButtonText style={styles.buttonText}>Créer un compte</ButtonText>
-                </TouchableOpacity>
+                    {/* Login button with navigation to login screen */}
+                    <TouchableOpacity style={styles.button} onPress={() => router.push('./../auth/authLogin/Login')}>
+                        <ButtonText style={styles.buttonText}>Se connecter</ButtonText>
+                    </TouchableOpacity>
+                    {/* Signup button with navigation to signup screen */}
+                    <TouchableOpacity style={styles.button} onPress={() => router.push('./../auth/authSignup/Signup')}>
+                        <ButtonText style={styles.buttonText}>Créer un compte</ButtonText>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
@@ -41,6 +53,9 @@ export default function LoginScreen() {
 }
 
 const styles = StyleSheet.create({
+    backgroundContainer: {
+        height: '60%',
+    },
     container: {
         backgroundColor: Colors.white,
         height: '40%',
