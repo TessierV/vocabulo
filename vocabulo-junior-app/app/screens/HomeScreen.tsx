d3f2db18222f8090ad67b3f2222f6d452b188d41
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 14:24:36 +0200

    home screen in the application

--- vocabulo-junior-app/app/screens/HomeScreen.tsx
+++ vocabulo-junior-app/app/screens/HomeScreen.tsx
@@ -1,21 +1,35 @@
+// HomeScreen.js
+// This file defines the home screen in the application,
+// displaying a navigation bar at the top and various home components.
+
 import { View, StyleSheet, SafeAreaView } from 'react-native';
 import React from 'react';
-import TopNavBar from '@/components/Navigation/TopNavBar';
-import GoToStart from '@/components/Home/GoToStart';
-import { Colors } from '@/constants/Colors';
-import DisplaySettings from '@/components/Home/DisplaySettings';
-import HelpToScannText from '@/components/Home/HelpToScannText';
+
+import { Colors } from '@/constants/Colors'; // Importing colors defined in constants
+
+import TopNavBar from '@/components/Navigation/TopNavBar'; // Importing the top navigation bar component
+import GoToStart from '@/components/Home/GoToStart'; // Importing the GotoStart component
+import DisplaySettings from '@/components/Home/DisplaySettings'; // Importing the displaySettings component
+import HelpToScannText from '@/components/Home/HelpToScannText'; // Importing the helpToScanText component
+
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
+      {/* Displaying the navigation bar with the title and specified colors */}
       <TopNavBar title="Accueil" tintColor={Colors.darkBlue} color={Colors.darkBlue} />
+
+      {/* Displaying the DisplaySettings component */}
       <View style={styles.DisplaySettingsComponent}>
         <DisplaySettings />
       </View>
+
+      {/* Displaying the GotoStart component */}
       <View style={styles.GoToStartComponent}>
         <GoToStart />
       </View>
+
+      {/* Displaying the HelpToScanText component */}
       <View style={styles.HelpToScanTextComponent}>
         <HelpToScannText />
       </View>
