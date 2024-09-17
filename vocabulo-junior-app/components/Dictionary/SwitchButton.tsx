cd5dc4d35672b4d1c79f8909483f527673cf424a
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 17:41:51 +0200

    Switch animated button to change alphabet filter

--- vocabulo-junior-app/components/Dictionary/SwitchButton.tsx
+++ vocabulo-junior-app/components/Dictionary/SwitchButton.tsx
@@ -1,34 +1,42 @@
+// This file defines the `SwitchButton` component, which features a rotating refresh icon.
+// When pressed, the button triggers a 360-degree rotation animation of the icon.
+
 import React, { useRef } from 'react';
 import { TouchableOpacity, StyleSheet, ViewStyle, Animated, GestureResponderEvent } from 'react-native';
+
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 import { Colors } from '@/constants/Colors';
 
-// Définir les types de props
+
+// Props interface for the SwitchButton component
 interface SwitchButtonProps {
-    onPress: (event: GestureResponderEvent) => void;
-    style?: ViewStyle;
+    onPress: (event: GestureResponderEvent) => void; // Function to call on button press
+    style?: ViewStyle; // Optional style for the button
 }
 
 const SwitchButton: React.FC<SwitchButtonProps> = ({ onPress, style }) => {
+    // Reference for the animated value
     const rotateAnim = useRef(new Animated.Value(0)).current;
 
+    // Handle button press with rotation animation
     const handlePress = (event: GestureResponderEvent) => {
         Animated.sequence([
             Animated.timing(rotateAnim, {
-                toValue: 1,
-                duration: 500,
+                toValue: 1, // Rotate to 360 degrees
+                duration: 500, // Duration of the rotation
                 useNativeDriver: true,
             }),
             Animated.timing(rotateAnim, {
-                toValue: 0,
-                duration: 0, // Pas de temps pour revenir à la position de départ
+                toValue: 0, // Return to original position
+                duration: 0, // Instant return
                 useNativeDriver: true,
             })
         ]).start();
 
-        onPress(event); // Appel du gestionnaire de pression
+        onPress(event); // Invoke the provided onPress handler
     };
 
+    // Interpolation for rotation effect
     const rotation = rotateAnim.interpolate({
         inputRange: [0, 1],
         outputRange: ['0deg', '360deg'],
