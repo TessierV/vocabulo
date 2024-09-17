bf74be1f18e6a2b33fb0653ce8637dbeb7a2a9f3
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 17:42:37 +0200

    displays user-specific settings

--- vocabulo-junior-app/components/Home/DisplaySettings.tsx
+++ vocabulo-junior-app/components/Home/DisplaySettings.tsx
@@ -1,24 +1,32 @@
+// This file defines the `DisplaySettings` component which displays user-specific settings and a greeting message.
+// It fetches the user's name from Firebase Auth or Firestore and provides navigation to the settings screen.
+
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { Colors } from '@/constants/Colors';
-import { ButtonText } from '@/constants/StyledText';
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { router } from 'expo-router';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Colors } from '@/constants/Colors';
+import { ButtonText } from '@/constants/StyledText';
+
+
 const DisplaySettings = () => {
+    // State to hold the user's name
     const [userName, setUserName] = useState<string>('Utilisateur');
 
     useEffect(() => {
+        // Function to fetch user data
         const fetchUserData = async () => {
             const user = auth().currentUser;
             if (user) {
-                // Attempt to get the display name from Firebase Auth
+                // Check if displayName is available from Firebase Auth
                 if (user.displayName) {
                     setUserName(user.displayName);
                 } else {
-                    // Fetch the user's name from Firestore if it's not available in Auth
+                    // Fetch user name from Firestore if displayName is not set
                     try {
                         const userDoc = await firestore().collection('users').doc(user.uid).get();
                         if (userDoc.exists && userDoc.data()?.name) {
@@ -37,12 +45,17 @@ const DisplaySettings = () => {
     return (
         <View style={styles.container}>
             <View style={styles.contents}>
-                <Image source={require('./../../assets/images/Logo-plum.png')} style={styles.logo} />
+                {/* Display logo image */}
+                <Image source={require('./../../assets/images/graphicElements/Logo-plum.png')} style={styles.logo} />
+
+                {/* Display greeting text with the user's name */}
                 <View style={styles.textContainer}>
                     <ButtonText style={styles.welcomeText}>
                         Bonjour <ButtonText style={styles.userName}>{userName}</ButtonText>
                     </ButtonText>
                 </View>
+
+                {/* Navigation to settings screen */}
                 <TouchableOpacity
                     onPress={() => router.push('./../../screens/SettingsScreen')}
                     style={styles.iconContainer}
