1c848f6e93d2dc1e63645290f88c7c18fa809469
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 11:01:23 +0200

    Dictionnary page, linked to the tab navigation bar and rendering DictionaryScreen

--- vocabulo-junior-app/app/(tabs)/Dictionary.tsx
+++ vocabulo-junior-app/app/(tabs)/Dictionary.tsx
@@ -1,15 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
-import DictionaryScreen from '../otherScreens/DictionaryScreen';
+// This file defines the Dictionnary page, which is linked to the tab navigation bar and displays the content of DictionaryScreen.
 
+import { View, StyleSheet } from 'react-native';
+import React from 'react';
+
+import DictionaryScreen from '../otherScreens/DictionaryScreen'; // Importing the DictionaryScreen component
 
 export default function Dictionnary() {
   return (
+    // Main container view that wraps the DictionaryScreen component
     <View style={styles.container}>
       <DictionaryScreen />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
