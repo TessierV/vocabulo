bb2d1e15e2fbe0def3e981002621538de28c9cb9
mandinou <113857342+Amandine4731@users.noreply.github.com>
2024-09-17 12:05:22 +0200

    Index component, which is the initial screen displayed when the app opens

--- vocabulo-junior-app/app/index.tsx
+++ vocabulo-junior-app/app/index.tsx
@@ -1,37 +1,20 @@
-import { Stack, Tabs, useNavigation } from 'expo-router';
-import { Text, View } from 'react-native';
-import { useEffect } from 'react';
-import HomeScreen from './screens/HomeScreen';
-import TopNavBar from '@/components/Navigation/TopNavBar';
-import LoginScreen from './screens/LoginScreen';
-import DictionnaryScreen from './otherScreens/DictionaryScreen';
-import Dictionnary from './(tabs)/Dictionary';
-import TakePhoto from './(tabs)/TakePhoto';
-import TakePhotoScreen from './otherScreens/TakePhotoScreen';
-import ParametersScreen from './screens/SettingsScreen';
-import TabLayout from './(tabs)/_layout';
-import SplashScreen from './screens/SplashScreen';
-import LogoSplash from '@/components/Splash/LogoSplash';
-import AllFilters from '@/components/Dictionary/AllFilters';
-import CustomProfile from '@/components/Settings/CustomProfile';
-import SettingsScreen from './screens/SettingsScreen';
-import OCRScannedTextScreen from '@/components/ScannedText/OCRScannedTextScreen';
+// This file defines the Index component, which is the initial screen displayed when the app opens.
+// It renders the ScreensLayout component, setting up the main navigation for the app.
+
+import { View } from 'react-native';
+
 import ScreensLayout from './screens/_layout';
-import RootLayout from './_layout';
+
+
 export default function Index() {
-  const navigation = useNavigation();
-
-  useEffect(() => {
-    navigation.setOptions({ headerShown: false });
-  }, [navigation]);
-
   return (
     <View
       style={{
         flex: 1
       }}
     >
-      <ScreensLayout/>
+      {/* Display the ScreensLayout navigation first when the app opens */}
+      <ScreensLayout />
     </View>
   );
 }
